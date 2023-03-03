42e7cd4bbb94a50011d29797a784285c665653f8
everwhatever <koszykarz.kuba@gmail.com>
2023-03-03 13:57:02 +0100

    add spec dir

---
+++ spec/User/Domain/Service/UserCreatorServiceSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\User\Domain\Service;
+
+use App\User\Domain\Service\UserCreatorService;
+use PhpSpec\ObjectBehavior;
+
+class UserCreatorServiceSpec extends ObjectBehavior
+{
+    public function testCreateUser()
+    {
+
+    }
+}
